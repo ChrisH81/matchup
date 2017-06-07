3c4baab524d8942400ce8fb58f19582f2452f69a
ChrisH81 <chrishurst1981@icloud.com>
2017-06-07 09:41:44 +0200

    add user pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name,  presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
